3112d9467d66dfad6a2150a22ce9f58a4464e3a0
cmlasalav <carlos.lasala.villegas@gmail.com>
2024-12-27 13:28:21 -0600

    More changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
+const path = require('path');
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  webpack(config) {
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
