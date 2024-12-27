ad36c8064e17fce8f04d4d1e92d31afb40f4e0c8
cmlasalav <carlos.lasala.villegas@gmail.com>
2024-12-27 13:18:09 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@
  */
 const nextConfig = {
   output: "export",
-  // Optional: Change the output directory `out` -> `dist`
   distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
