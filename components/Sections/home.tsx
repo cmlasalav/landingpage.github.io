eb3946839292a8138ad7369c143c1d6d6a34ef13
cmlasalav <carlos.lasala.villegas@gmail.com>
2024-12-28 11:57:41 -0600

    Security Changes

--- components/Sections/home.tsx
+++ components/Sections/home.tsx
@@ -9,7 +9,7 @@ import { langContext } from "../../context/langContext";
 import Logo from "../../public/images/Logo-White.jpg";
 //import Loading
 
-const HomeURL = "https://landingpage-backend-one.vercel.app/home";
+const HomeURL = `${process.env.REACT_APP_API_URL}/home`;
 
 export default function GeneraHome() {
   const [home, setHome] = useState([]);
