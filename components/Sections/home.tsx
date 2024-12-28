49b7b6f3e9919ec3f6284be06b07c59599216e93
cmlasalav <carlos.lasala.villegas@gmail.com>
2024-12-28 12:30:11 -0600

    Security changes

--- components/Sections/home.tsx
+++ components/Sections/home.tsx
@@ -9,7 +9,7 @@ import { langContext } from "../../context/langContext";
 import Logo from "../../public/images/Logo-White.jpg";
 //import Loading
 
-const HomeURL = `${process.env.REACT_APP_API_URL}/home`;
+const HomeURL = `${process.env.NEXT_PUBLIC_API_UR}/home`;
 
 export default function GeneraHome() {
   const [home, setHome] = useState([]);
